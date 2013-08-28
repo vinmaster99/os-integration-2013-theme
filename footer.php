14ddf75f6940831ab36660aa05b5dc476183c888
vinmaster99 <yho@onescreen.com>
2013-08-28 12:16:33 -0700

    Add back footer div and update css

--- footer.php
+++ footer.php
@@ -1,7 +1,9 @@
 		<footer>
 			<div class="container">
-				<div class="row-fluid">
-					<?php dynamic_sidebar('footer-widgets'); ?>
+				<div class="footer-content">
+					<div class="row-fluid">
+						<?php dynamic_sidebar('footer-widgets'); ?>
+					</div>
 				</div>
 			</div>
 			<?php //wp_nav_menu( array('menu' => 'Footer Navigation', 'container' => false, 'menu_class' => 'nav nav-pills')); ?>
