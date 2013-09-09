ba8360be45af69fc7118bb43dd4015e117d97ed8
vinmaster99 <yho@onescreen.com>
2013-09-09 12:07:54 -0700

    Allow search by ID in the default post search bar

--- functions.php
+++ functions.php
@@ -468,4 +468,31 @@ function social_div_text() {
 	return $content;
 }
 
+// Allow searching post by ID
+function idsearch( $wp ) {
+    global $pagenow;
+
+    // If it's not the post listing return
+    if( 'edit.php' != $pagenow )
+        return;
+
+    // If it's not a search return
+    if( !isset( $wp->query_vars['s'] ) )
+        return;
+
+    // If it's a search but there's no prefix, return
+    if( '#' != substr( $wp->query_vars['s'], 0, 1 ) )
+        return;
+
+    // Validate the numeric value
+    $id = absint( substr( $wp->query_vars['s'], 1 ) );
+    if( !$id )
+        return; // Return if no ID, absint returns 0 for invalid values
+
+    // If we reach here, all criteria is fulfilled, unset search and select by ID instead
+    unset( $wp->query_vars['s'] );
+    $wp->query_vars['p'] = $id;
+}
+add_action( 'parse_request', 'idsearch' );
+
 ?>
