b97c33d7a72979e84f46812b5e75477f7f9e2353
vinmaster99 <yho@onescreen.com>
2013-09-11 16:26:45 -0700

    More fixing css of footer

--- footer.php
+++ footer.php
@@ -1,17 +1,17 @@
 		<footer>
-			<div class="container">
-			<div class="footer footer-login-page">
-				<div class="footer-login-page-wrap">
-					<?php wp_nav_menu( array('menu' => 'Footer Navigation', 'container' => 'false', 'menu_class' => 'footer-nav')); ?>
-					<ul class="footer-nav clear">
-						<li>OneScreen Inc © 2013 All Rights Reserved</li>
-						<li><a href="http://legal.onescreen.com/user/terms-of-use/">Terms &amp; Conditions</a></li>
-						<li><a href="http://legal.onescreen.com/user/copyright-policy/">Copyright</a></li>
-						<li><a href="http://legal.onescreen.com/user/privacy-policy/">Privacy Policy</a></li>
-						<li><a href="http://legal.onescreen.com/user/opt-out/">Opt Out</a></li>
-					</ul>
+			<div class="container clear">
+				<div class="footer-login-page">
+					<div class="footer-login-page-wrap">
+						<?php wp_nav_menu( array('menu' => 'Footer Navigation', 'container' => 'false', 'menu_class' => 'footer-nav')); ?>
+						<ul class="footer-nav copyright">
+							<li>OneScreen Inc © 2013 All Rights Reserved</li>
+							<li><a href="http://legal.onescreen.com/user/terms-of-use/">Terms &amp; Conditions</a></li>
+							<li><a href="http://legal.onescreen.com/user/copyright-policy/">Copyright</a></li>
+							<li><a href="http://legal.onescreen.com/user/privacy-policy/">Privacy Policy</a></li>
+							<li><a href="http://legal.onescreen.com/user/opt-out/">Opt Out</a></li>
+						</ul>
+					</div>
 				</div>
-			</div>
 			</div>
 		</footer>
 		<?php wp_footer(); ?>
